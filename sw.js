68eab7629832c4a9459b2e66c1ac83d0053a8cc4
ys-a11y-cjj <yuanshu@m2.csghs.tp.edu.tw>
2026-04-17 12:36:32 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'teacher-app-v1';
+const ASSETS = ['./','./index.html','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Google API 請求一律不走快取
+  if (url.hostname.includes('google') || url.hostname.includes('googleapis')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
